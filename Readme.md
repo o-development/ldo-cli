b523f9a47fef6ab14d1a402ec1e66db9229474b9
jaxoncreed <jaxoncreed@gmail.com>
2022-06-18 13:33:06 -0700

    Initialized cli

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
-# Solid-Dataset
+# LDO-CLI
 
-## Notes:
- - Any quads in the default graph will not be synced to Solid Pods, but will be added to the dataset without any syncing
+```
+ldo-cli build --input /shapes --output /ldo
+```
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .command('build')
+  .option('-i, --input <inputPath>')
+  .option('-o, --output <outputPath>');
+
+program.parse();
