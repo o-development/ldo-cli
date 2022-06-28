5f114863e7b0f643d7d52cf20177b600bcaafb96
jaxoncreed <jaxoncreed@gmail.com>
2022-06-27 20:11:59 -0400

    Templating

---
+++ lib/build.ts
@@ -0,0 +1,57 @@
+import fs from "fs-extra";
+import path from "path";
+import { Schema } from "shexj";
+import parser from "@shexjs/parser";
+import shexjToTypeAndContext from "shexj2typeandcontext";
+import { renderFile } from "ejs";
+
+interface BuildOptions {
+  input: string;
+  output: string;
+}
+
+export async function build(options: BuildOptions) {
+  const shapeDir = await fs.promises.readdir(options.input, {
+    withFileTypes: true,
+  });
+  // Filter out non-shex documents
+  const shexFiles = shapeDir.filter(
+    (file) => file.isFile() && file.name.endsWith(".shex")
+  );
+  // Prepare new folder by clearing/andor creating it
+  if (fs.existsSync(options.output)) {
+    await fs.promises.rm(options.output, { recursive: true });
+  }
+  await fs.promises.mkdir(options.output);
+
+  await Promise.all(
+    shexFiles.map(async (file) => {
+      const fileName = path.parse(file.name).name;
+      // Get the content of each document
+      const shexC = await fs.promises.readFile(
+        path.join(options.input, file.name),
+        "utf8"
+      );
+      // Convert to ShexJ
+      const schema: Schema = parser
+        .construct("https://ldo.js.org/")
+        .parse(shexC);
+      // Convert the content to types
+      const [typings, context] = await shexjToTypeAndContext(schema);
+      const finalContent = await renderFile(
+        path.join(__dirname, "buildOutput.ejs"),
+        {
+          typings,
+          fileName,
+          schema: JSON.stringify(schema, null, 2),
+          context: JSON.stringify(context, null, 2),
+        }
+      );
+      // Save conversion to document
+      await fs.promises.writeFile(
+        path.join(options.output, `${fileName}.ts`),
+        finalContent
+      );
+    })
+  );
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { program } from "commander";
+import { build } from "./build";
 
 program
-  .command('build')
-  .option('-i, --input <inputPath>')
-  .option('-o, --output <outputPath>');
+  .name("LDO-CLI")
+  .description("CLI to some JavaScript string utilities")
+  .version("0.0.1");
+
+program
+  .command("build")
+  .description("Build a shex folder into Shape Types")
+  .option("-i, --input <inputPath>", "Provide the input path", "./shapes")
+  .option("-o, --output <outputPath>", "Provide the output path", "./ldo")
+  .action(build);
 
 program.parse();
